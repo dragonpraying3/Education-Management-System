35ddf3bfbfc239b7392f88073606db9831fc83b0
jinming060716 <79146043+jinming060716@users.noreply.github.com>
2025-02-26 21:23:53 +0800

    Add files via upload

---
+++ student/student_menu.py
@@ -0,0 +1,33 @@
+import upd_info
+import course_enrol
+import material_access
+
+
+
+def student_menu():
+    while True:
+        print("\n-------------------------------")
+        print("---------Student Menu----------")
+        print("-------------------------------")
+        print("1. Update Personal Information")
+        print("2. Course Enrolment")
+        print("3. Course Material Access")
+        print("4. Grades Tracking")
+        print("5. Feedback Submission")
+        print("6. Exit")
+        print("-------------------------------")
+
+        opt = int(input("\nYour choice: "))
+
+        if opt == 1:
+            upd_info.information_menu()
+        elif opt == 2:
+            course_enrol.course_enrolment_menu()
+        elif opt == 3:
+            material_access.course_material_access()
+        elif opt == 6:
+            print("Exiting")
+            break
+
+
+student_menu()
