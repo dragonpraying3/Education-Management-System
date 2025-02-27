1b18f8932b26c68e8eb89e87db16a1583d926ee5
jinming060716 <79146043+jinming060716@users.noreply.github.com>
2025-02-27 21:34:33 +0800

    Add files via upload

---
+++ student/feedback_submission.py
@@ -0,0 +1,97 @@
+def open_mailbox():
+    mailbox=[] #create an empty list to store data
+    with open("mail.txt", 'r')as message:
+        for line in message:
+            line=line.rstrip().split(',') #remove whitespace and split each line become a list
+            #store each student list in a dictionary
+            detail={
+                'Name':line[0],
+                'Role':line[1],
+                'Message':line[2],
+                'Reply':line[3]if len(line) > 3 else ""  #add reply column if having reply message else empty
+            }
+            mailbox.append(detail) #add dictionary into mailbox list
+    return mailbox #return list containing dictionary into mailbox
+
+def open_students():
+    students=[] #create an empty list to store student data
+    with open("students.txt", 'r') as tFile:
+        for line in tFile:
+            line=line.rstrip().split(",") #split each line become a list and remove whitespace
+            #set the limit of the append block inside the list
+            while len(line)<9:
+                line.append("")
+            #store each list in a dictionary
+            student = {
+                "Student ID": line[0],
+                "Name": line[1],
+                "Email": line[2],
+                "Contact": line[3],
+                "Emergency Contact": line[4],
+                "Gender":line[5],
+                "Student Status":line[6],
+                "Tuition Fees":line[7],
+                "Payment":line[8]
+            }
+            students.append(student) #add student dictionary to the list
+    return students #return the list containing student dictionary
+
+def feedback_menu():
+    while True:
+        print("")
+        title = "Communication Page"
+        width = 40
+        print("=" * width)
+        print(title.center(width))
+        print("=" * width)
+        print("1. Submit a feedback")
+        print("2. Exit")
+
+        opt = int(input("Select an option: "))
+
+        if opt == 1:
+            submit_feedback()
+        elif opt ==2:
+            break
+        else:
+            print("Invalid choice, please enter a valid number (1-2).")
+            return
+
+
+def submit_feedback():
+    print("\nSubmit Feedback")
+
+    students = open_students()
+    mailboxes = open_mailbox()
+
+    tp_number = input("\nEnter your TP number: ").upper()
+    student_found = None
+    for student in students:
+        if student["Student ID"] == tp_number:
+            student_found = student
+            break  # stop when found student id
+
+    if student_found:
+        name = input("Enter your name: ").strip()
+        msg = input("Enter your feedback message: ").strip()
+        role = "Student"  # keep the role to student
+
+        # new submission dictionary
+        new_submission = {
+            "Name": name,
+            "Role": role,
+            "Message": msg,
+            "Reply": ""  # reply only for staff , keep it empty
+        }
+
+        mailboxes.append(new_submission)
+        with open("mail.txt", "w") as message:
+            for mail in mailboxes:
+                message.write(",".join(mail.values()) + "\n")
+
+        print("Feedback submitted successfully!")
+    else:
+        input("Student ID not found, make sure enter correct student ID.")
+
+
+# feedback_menu()
