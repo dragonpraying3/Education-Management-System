114d351030b6885ee9539e6622c74d097aa0bf14
dragonlovecode <longjialing0305@gmail.com>
2025-03-01 17:21:27 +0800

    Update event_management.py

--- staff/event_management.py
+++ staff/event_management.py
@@ -1,165 +1,184 @@
-def open_resource():
-    resources=[] #empty list to store data
+from datetime import datetime
+
+def open_event(): #read event.txt file
+    events=[]
     try:
-        with open("resource.txt",'r')as gFile:
-            for line in gFile:
+        with open("event.txt",'r')as vFile:
+            for line in vFile:
                 line=line.rstrip().split(",")#remove whitespace and split each line become a list
-                #index label and store in dictionary
-                item={
-                    "Classroom":line[0],
-                    "Resource":line[1],
-                    "Number":line[2],
-                    "Available":line[3]
+                #store each list in a dictionary with index label
+                program={
+                    "Category":line[0],
+                    "Title":line[1],
+                    "Date":line[2],
+                    "Duration":line[3],
+                    "Venue":line[4],
+                    "Leader":line[5]
                 }
-                resources.append(item)
-            return resources
+                events.append(program) #add each event program to the events list
+            return events
     except FileNotFoundError:
-        print("Error: resource.txt not found.")
-    return None
+        print("Warning: 'event.txt' not found. ")
+    return None #return an empty list is file is missing
 
-def update_resource():
-    resources=open_resource()
-    if resources is None:
+def update_event():
+    events=open_event()
+    if events is None:
         return
-    with open("resource.txt", 'w') as gFile:
-        for resource in resources:
-            gFile.write(",".join(resource.values())+"\n")
-            print(f"Classroom: {resource['Classroom']} \t\t Resource: {resource['Resource']} \t\t Quantity: {resource['Number']}\n")
 
-def continue_searching():
-    while True:
-        try:
-            proceed = input("\nContinue searching?(Y/N):").capitalize()
-            if proceed == 'Y':
-                return True
-            elif proceed == 'N':
-                print("Thank you for searching!")
-                return False
-            else:
-                raise ValueError("Invalid input. Please enter only Y or N")
-        except ValueError as e:
-            print(e)
+    with open("event.txt", 'w') as vFile:
+        index = 1
+        for event in events:
+            vFile.write(",".join(event.values()) + "\n")
+            print(f"Category:{event['Category']}\n")
+            print("Event details:")
+            print(f"{index}. Title: {event['Title']:<50}\tDate: {event['Date']:<10}\tVenue: {event['Venue']:<7}"
+                  f"\tDuration: {event['Duration']:<10}\tLeader: {event['Leader']:<10}")
+            index += 1
+            print("-" * 150)
 
-def search_by_classroom():
-    resources=open_resource()
-    if resources is None:
+def register_event():
+    events = open_event()
+    if events is None:
         return
+
     while True:
-        classroom=input("Enter the classroom name:").capitalize()
-        found=False #track a match when found
-
-        for resource in resources: #check the classroom one by one
-            if resource['Classroom']==classroom:
-                print(f"Classroom {classroom} contain {resource['Resource']} with quantity {resource['Number']}")
-                found=True
-
-        if not found:
-            print("Sorry,classroom entered is not found")
-        if not continue_searching():
-            break
-
-def search_by_resource():
-    resources=open_resource()
-    if resources is None:
-        return
-    while True:
-        resource_enter=input("Enter the resource name:").capitalize()
-        found=False #track if the resource is found
-        for resource in resources:
-            if resource['Resource']==resource_enter:
-                print(f"The {resource_enter} is in {resource['Classroom']} with quantity {resource['Number']}")
-                found=True #mark as found
-        if not found:
-            print("Sorry,classroom entered is not found")
-        if not continue_searching():
-            break
-
-def resource_allocation():
-    resources=open_resource()
-    update_resource()
-    if resources is None:
-        return
-    while True:
-        classroom=input("Enter the classroom to allocate resource:").title()
-        class_found=False
-        for resource in resources:
-            if classroom ==resource['Classroom']:
-                class_found=True
-                break
-        if not class_found:
-            print(f"Classroom {classroom} not found. Please enter again\n")
-            continue
+        print("-" * 50, "REGISTER NEW EVENT", "-" * 50)
 
         while True:
-            allocate_resource=input("Enter the resource name need to allocate:").capitalize()
-            found_resource=False
-            for resource in resources:
-                if resource['Classroom']==classroom and resource['Resource']==allocate_resource:
-                    found_resource=True
-                    break
-            if not found_resource:
-                print(f"Resource '{allocate_resource}' not found in {classroom}. Please enter a valid resource.\n")
+            category=input("Enter event category(Academic/Extracurricular/Conference/Seminar):").capitalize()
+            # validate the input of user is within the listed category
+            if category not in ["Academic","Extracurricular","Conference","Seminar"]:
+                print("Entered category is not found.\n")
                 continue
-            break
+            else:
+                break
 
+        title=input("Enter event title:").title().strip()
         while True:
             try:
-                quantity = int(input(f"Enter the quantity of {allocate_resource} to allocate: "))
-                if quantity <= 0:
-                    raise ValueError("Quantity must be greater than 0!")
-                break  # Exit loop if valid
-            except ValueError as e: #e is the error that made by the user
-                print(f"Invalid input: {e}")
-
-        done_allocate=False
-        for resource in resources:
-            if resource['Resource']==allocate_resource:
-                available=int(resource['Available']) #convert string into integer
-                number=int(resource['Number']) #convert string into integer
-                if quantity > available:
-                    print(f"\nNot enough {allocate_resource}. Only {available} left.")
-                    break #print one time available message
-                else:
-                    resource['Available'] = str(available - quantity) #deduct allocated quantity from available
-                    resource['Number']=str(number+quantity) #updated allocated number
-                    done_allocate=True
+                date=input("Enter event date (dd/mm/yyyy):").strip() #validate the date format
+                datetime.strptime(date,"%d/%m/%Y")
                 break
+            except ValueError:
+                print("Invalid date format! Please enter the date in DD/MM/YYYY format.\n")
 
-        if done_allocate:
-            with open("resource.txt", 'w') as gFile:
-                for resource in resources:
-                    gFile.write(",".join(resource.values()) + "\n")
+        duration=input("Enter event duration(h/m):").strip()
+        venue=input("Enter event venue:").strip().capitalize()
+        leader=input("Enter event leader:").strip().title()
 
-            print("Update Resource List:")
-            update_resource()
+        new_event=f"{category},{title},{date},{duration},{venue},{leader}"
+        with open("event.txt",'a')as vFile:
+            vFile.write(new_event+"\n")
 
-        proceed_again=input("\nProceed allocated?(Y/N):").capitalize()
-        if proceed_again == 'N':
-            print("Resource allocation completed!")
+        print("Register successful!\n")
+        print("-" * 50, "Update Event List", "-" * 50)
+        update_event()
+
+        proceed= input("Proceed register?(Y/N):").capitalize()
+        if proceed == 'N':
+            print("Event Register Completed!\nThank You for Visiting!")
             return
 
-def resource_allocation_menu():
+def manage_event():
+    events=open_event()
+    if events is None:
+        return
     while True:
         print("")
-        title = "Resource Allocation Page"
+        print("-" * 50, "Action Page", "-" * 50)
+        print("1. Delete Event")
+        print("2. Edit Event Content")
+        print("3. Skip the step")
+
+        try:
+            selection = int(input("\nPlease Enter your choice:"))
+            print("-" * 50, "DELETING EVENT PAGE", "-" * 50)
+            if selection == 1:
+                update_event()
+                line_number=int(input("Enter the number of event need to be delete:"))
+                if 1<= line_number <= len(events):
+                    #delete the real number in the list
+                    del events[line_number-1]
+
+                    #update the delete list to the file
+                    with open("event.txt",'w')as vFile:
+                        for event in events:
+                            vFile.write(",".join(event.values())+"\n")
+
+                    print("Event deleted!\n")
+                    print("-" * 50, "Update Event List", "-" * 50)
+                    update_event()
+                else:
+                    print("The number is not in event list.")
+                    continue
+            elif selection == 2:
+                update_event()
+                while True:
+                    try:
+                        num = int(input("Enter the number of the event edit: "))
+                        if 1 <= num <= len(events):
+                            event=events[num-1] #get the selected event dictionary from list
+                            part = input("Please enter the part of the event to edit(Title/Date/Venue/Duration/Leader):").title()
+                            if part not in event:
+                                print("The part you enter is not found in the event list. Please enter again.\n")
+                                continue
+
+                            # edit the part to whatever new
+                            edition = input(f"Edit {part} to :").strip().title()
+
+                            if part == "Date":
+                                try:
+                                    # check if the date is input as correct format
+                                    datetime.strptime(edition, "%d/%m/%Y")
+                                except ValueError:
+                                    print("Invalid date format! Use DD/MM/YYYY.")
+                                    continue
+
+                            event[part] = edition #change the part to edition entered
+                            with open("event.txt",'w')as vFile:
+                                for event in events:
+                                    vFile.write(",".join(event.values())+"\n")
+
+                            print("Event edited!\n")
+                            print("-" * 50, "Update Event List", "-" * 50)
+                            update_event()
+                            break
+                        else:
+                            print("Invalid number. Please enter bill in the event list!")
+                    except ValueError:
+                        print("Invalid input. Please enter a valid number.\n")
+
+            elif selection==3:
+                print("Skipping Page...")
+                break
+            else:
+                print("Please enter number 1-3 only.")
+        except ValueError:
+            print("Invalid input. Only integer 1-3 is allowed.")
+
+def event_menu():
+    while True:
+        print("")
+        title = "Event Management Page"
         width = 40
         print("=" * width)
         print(title.center(width))
         print("=" * width)
-        print("1. Search by class name")
-        print("2. Search by resource name")
-        print("3. Allocate resource")
+        print("1. View Event")
+        print("2. Register Event")
+        print("3. Manage Event")
         print("4. Exit")
 
         try:
-            choice=int(input("Enter your choice:"))
-            if choice ==1:
-                search_by_classroom()
-            elif choice==2:
-                search_by_resource()
-            elif choice==3:
-                resource_allocation()
-            elif choice==4:
+            choose=int(input("\nEnter your action:"))
+            if choose ==1 :
+                update_event()
+            elif choose==2:
+                register_event()
+            elif choose==3:
+                manage_event()
+            elif choose==4:
                 print("Thank you for visiting!")
                 break
             else:
@@ -167,4 +186,5 @@ def resource_allocation_menu():
         except ValueError:
             print("Invalid input! Only integer 1-3 is allowed.")
 
-# resource_allocation_menu()
+
+# event_menu()
